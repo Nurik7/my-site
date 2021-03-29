655c04e82899e1345ef17578baee50d8f39e9732
Nurik7 <tashpulatov00@mail.ru>
2021-03-30 02:53:22 +0500

    Added new site and some changes in main page

---
+++ source/joomhost/js/main.js
@@ -0,0 +1,22 @@
+﻿$(function () {
+    $('.reviews-slider').slick({
+        infinite: true,
+        slidesToShow: 2,
+        slidesToScroll: 2,
+        dots: true,
+        arrows: false,
+        responsive: [
+            {
+                breakpoint: 1106,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                }
+            },
+        ]
+    });
+
+    $('.btn__menu').on('click', function () {
+        $('.menu__list').slideToggle();
+    });
+});
