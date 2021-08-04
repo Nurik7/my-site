098d3242bc7d0150a2742b966a340364179a71d1
Nurik7 <tashpulatov00@mail.ru>
2021-08-05 02:24:29 +0500

    Add files via upload
    Added new site make-up

---
+++ source/free-psd/js/main.js
@@ -0,0 +1,11 @@
+$(function () {
+
+    $('.slider__inner').slick({
+        prevArrow: '<button type="button" class="slick-btn slick-prev"></button>',
+        nextArrow: '<button type="button" class="slick-btn slick-next"></button>'
+    });
+    $('.news__slider-inner').slick({
+        prevArrow: '<button type="button" class="slick-btn slick-prev"></button>',
+        nextArrow: '<button type="button" class="slick-btn slick-next"></button>'
+    });
+});
